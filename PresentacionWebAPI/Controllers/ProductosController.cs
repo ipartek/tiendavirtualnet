1671d18dd2e475805f1b1c18ae7b4de530ea24b5
javierlete <jglete@gmail.com>
2018-03-12 15:48:44 +0100

    WebAPI Obtener Todos los Productos

---
+++ PresentacionWebAPI/Controllers/ProductosController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TiendaVirtual.Entidades;
+using TiendaVirtual.LogicaNegocio;
+
+namespace PresentacionWebAPI.Controllers
+{
+    public class ProductosController : ApiController
+    {
+        // GET: api/Productos
+        public IEnumerable<IProducto> Get()
+        {
+            LogicaNegocio ln = new LogicaNegocio();
+            return ln.ListadoProductos();
+        }
+
+        // GET: api/Productos/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Productos
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Productos/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Productos/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
